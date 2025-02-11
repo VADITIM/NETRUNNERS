2cd85f9c93f028070b3421e2ee9befedc4331e6b
Vadim Niedental <vadim.niedental@gmail.com>
2025-02-11 21:31:10 +0100

    Character Abilities.

---
+++ Scripts/Abilities/CharacterOneAbilities.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CharacterOneAbilities : Abilities
+{
+    public override void AbilityOne()
+    {
+        Debug.Log("CharacterOne used Ability 1!");
+    }
+
+    public override void AbilityTwo()
+    {
+        Debug.Log("CharacterOne used Ability 2!");
+    }
+}
---
+++ Scripts/Abilities/CharacterTwoAbilities.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CharacterTwoAbilities : Abilities
+{
+    public override void AbilityOne()
+    {
+        Debug.Log("CharacterTwo used Ability 1!");
+    }
+
+    public override void AbilityTwo()
+    {
+        Debug.Log("CharacterTwo used Ability 2!");
+    }
+}
