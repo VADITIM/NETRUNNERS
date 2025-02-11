4520d7e7a44f1724db80478aacc96e9d047fcf40
Vadim Niedental <vadim.niedental@gmail.com>
2025-02-11 21:31:45 +0100

    Added a selection of Characters for each Client after Clientstartup.

---
+++ Scripts/Characters/Miscellaneous/CharacterSelection.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using FishNet.Object;
+using FishNet.Connection;
+using FishNet.Object.Synchronizing;
+using FishNet.Managing;
+
+public class CharacterSelection : NetworkBehaviour
+{
+    [SerializeField] private GameObject[] characters;
+    [SerializeField] private GameObject characterSelectionPanel;
+    [SerializeField] private GameObject canvas;
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        if (base.IsOwner)
+        {
+            canvas.SetActive(true);
+        }
+        else
+        {
+            canvas.SetActive(false); 
+        }
+    }
+
+    public void SpawnSamus()
+    {
+        if (!IsOwner) return; 
+        characterSelectionPanel.SetActive(false);
+        SpawnCharacter(0, base.Owner);
+    }
+
+    public void SpawnRonin()
+    {
+        if (!IsOwner) return; 
+        characterSelectionPanel.SetActive(false);
+        SpawnCharacter(1, base.Owner);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SpawnCharacter(int spawnIndex, NetworkConnection conn)
+    {
+        GameObject character = Instantiate(characters[spawnIndex], SpawnPoint.instance.transform.position, Quaternion.identity);
+        Spawn(character.GetComponent<NetworkObject>(), conn);
+    }
+}
