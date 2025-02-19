92c4f63716e5f2738d455a4601b8fd21b5fa8b8a
Vadim Niedental <vadim.niedental@gmail.com>
2025-02-19 22:54:00 +0100

    Divided into regions - Several State Machine States - Server Syncinc

--- Scripts/Characters/CharacterBase.cs
+++ Scripts/Characters/CharacterBase.cs
@@ -4,35 +4,35 @@ using System.Collections;
 
 public abstract class CharacterBase : NetworkBehaviour
 {
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private SpriteRenderer sprite;
-    [SerializeField] private Animator animator;
-
-    private float speed = 3.5f;
-    private float jumpForce = 5f;
-    private float acceleration = 20f;
-    private float maxSpeed = 8.5f;
-    private float jumpBoostX = 10f;
-    [SerializeField] private float groundCheckDistance = 0.18f;
-
     public int PlayerID { get; private set; }
 
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private LayerMask groundLayer;
+
+    private float speed = 3.5f;
+    private float maxSpeed = 8.5f;
+    private float acceleration = 20f;
+    private float jumpForce = 8f;
+    private float jumpBoostX = 10f;
+    private float groundCheckDistance = 0.18f;
+
+    public bool isAttacking;
+    public float normalAttackFrames = .682f;
+    public float specialAttackFrames = .932f;
+    [SerializeField] private GameObject weaponInstance;
+    
     public GameObject weaponPrefab;
     public Transform weaponHolder;
-    public GameObject weaponInstance;
-
+    private Vector3 weaponHolderPosition;
     private Rigidbody rb;
+
     private Movement movement;
     public Abilities abilities; 
-    
     private StateMachine stateMachine;
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-        abilities = GetComponent<Abilities>(); 
-    }
 
+#region FishNet Methods
     public override void OnStartClient()
     {
         base.OnStartClient();
@@ -40,6 +40,7 @@ public abstract class CharacterBase : NetworkBehaviour
         if (IsOwner)
         {
             movement = new Movement(rb, sprite, groundLayer, speed, jumpForce, groundCheckDistance, acceleration, maxSpeed);
+            stateMachine = new StateMachine(movement, animator, this);
             gameObject.tag = "Player1";
         }
         else
@@ -68,7 +69,226 @@ public abstract class CharacterBase : NetworkBehaviour
             weaponHolder.localRotation = Quaternion.identity;
         }
     }
-    
+#endregion
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        abilities = GetComponent<Abilities>(); 
+        weaponHolderPosition = weaponHolder.localPosition;
+    }
+
+    protected virtual void Update()
+    {
+        if (IsOwner && stateMachine != null)
+        {
+            stateMachine.UpdateState();
+        }
+
+        if (movement == null) return;
+        movement.FixedUpdate();
+    }
+
+    public void MirrorWeaponHolderPosition(bool mirrored)
+    {
+        if (weaponHolder == null) return;
+
+        Vector3 position = mirrored ? new Vector3(-weaponHolderPosition.x, weaponHolderPosition.y, weaponHolderPosition.z) : weaponHolderPosition;
+        weaponHolder.localPosition = position;
+    }
+
+    public void InitialWeaponHolderPosition(bool initial)
+    {
+        if (weaponHolder == null) return;
+
+        Vector3 position = initial ? weaponHolderPosition : new Vector3(-weaponHolderPosition.x, weaponHolderPosition.y, weaponHolderPosition.z);
+        weaponHolder.localPosition = position;
+    }
+
+
+
+    // Request state update
+    public void RequestStateUpdate(bool isMoving, bool isJumping)
+    {
+        RequestStateUpdateServerRpc(isMoving, isJumping);
+    }
+
+    [ServerRpc]
+    private void RequestStateUpdateServerRpc(bool isMoving, bool isJumping)
+    {
+        ApplyStateUpdateObserversRpc(isMoving, isJumping);
+    }
+
+    [ObserversRpc]
+    private void ApplyStateUpdateObserversRpc(bool isMoving, bool isJumping)
+    {
+        animator.SetBool("isMoving", isMoving);
+        animator.SetBool("isJumping", isJumping);
+    }
+
+    // Request attack state update
+    public void RequestAttackState(AttackType attackType)
+    {
+        RequestAttackStateServerRpc(attackType);
+    }
+
+    [ServerRpc]
+    private void RequestAttackStateServerRpc(AttackType attackType)
+    {
+        ApplyAttackStateObserversRpc(attackType);
+    }
+
+    [ObserversRpc]
+    private void ApplyAttackStateObserversRpc(AttackType attackType)
+    {
+        switch (attackType)
+        {
+            case AttackType.NormalGround:
+                StartCoroutine(NormalAttackRoutine());
+                break;
+            case AttackType.SpecialGround:
+                StartCoroutine(SpecialAttackRoutine());
+                break;
+            case AttackType.NormalAir:
+                StartCoroutine(NormalAirAttackRoutine());
+                break;
+            case AttackType.SpecialAir:
+                StartCoroutine(SpecialAirAttackRoutine());
+                break;
+        }
+    }
+
+#region Flip Sprite
+    public void FlipSprite(float x, float z)
+    {
+        if (x > 0)
+        {
+            RequestFlipSpriteServer(false);
+        }
+        else if (x < 0)
+        {
+            RequestFlipSpriteServer(true);
+        }
+    }
+
+    [ServerRpc]
+    private void RequestFlipSpriteServer(bool flip)
+    {
+        ApplyFlipSpriteObservers(flip);
+    }
+
+    [ObserversRpc]
+    private void ApplyFlipSpriteObservers(bool flip)
+    {
+        sprite.flipX = flip;
+        MirrorWeaponHolderPosition(flip);
+        InitialWeaponHolderPosition(!flip);
+    }
+#endregion
+
+#region Attack Routines
+    private IEnumerator NormalAttackRoutine()
+    {
+        isAttacking = true;
+        animator.SetBool("isNormalAttacking", true);
+
+        yield return new WaitForSeconds(normalAttackFrames);
+
+        isAttacking = false;
+        animator.SetBool("isNormalAttacking", false);
+    }
+
+    private IEnumerator SpecialAttackRoutine()
+    {
+        isAttacking = true;
+        animator.SetBool("isSpecialAttacking", true);
+
+        yield return new WaitForSeconds(specialAttackFrames);
+
+        isAttacking = false;
+        animator.SetBool("isSpecialAttacking", false);
+    }
+
+    private IEnumerator NormalAirAttackRoutine()
+    {
+        isAttacking = true;
+        animator.SetBool("isAirNormalAttacking", true);
+
+        yield return new WaitForSeconds(normalAttackFrames);
+
+        isAttacking = false;
+        animator.SetBool("isAirNormalAttacking", false);
+    }
+
+    private IEnumerator SpecialAirAttackRoutine()
+    {
+        isAttacking = true;
+        animator.SetBool("isAirSpecialAttacking", true);
+
+        yield return new WaitForSeconds(specialAttackFrames);
+
+        isAttacking = false;
+        animator.SetBool("isAirSpecialAttacking", false);
+    }
+#endregion
+
+#region Start Attack Methods
+    public void StartNormalAttack()
+    {
+        isAttacking = true;
+        animator.SetBool("isNormalAttacking", true);
+    }
+
+    public void StartSpecialAttack()
+    {
+        isAttacking = true;
+        animator.SetBool("isSpecialAttacking", true);
+    }
+
+    public void StartAirNormalAttack()
+    {
+        isAttacking = true;
+        animator.SetBool("isAirNormalAttacking", true);
+    }
+
+    public void StartAirSpecialAttack()
+    {
+        isAttacking = true;
+        animator.SetBool("isAirSpecialAttacking", true);
+    }
+#endregion
+
+#region End Attack Methods
+    public void EndNormalAttack()
+    {
+        isAttacking = false;
+        animator.SetBool("isNormalAttacking", false);
+        animator.Play("idle");
+    }
+
+    public void EndSpecialAttack()
+    {
+        isAttacking = false;
+        animator.SetBool("isSpecialAttacking", false);
+        animator.Play("idle");
+    }
+
+    public void EndAirNormalAttack()
+    {
+        isAttacking = false;
+        animator.SetBool("isAirNormalAttacking", false);
+        animator.Play("idle");
+    }
+
+    public void EndAirSpecialAttack()
+    {
+        isAttacking = false;
+        animator.SetBool("isAirSpecialAttacking", false);
+        animator.Play("idle");
+    }
+#endregion
+
+#region Death Methods   
     [ServerRpc]
     public void DisablePlayerServerRpc()
     {
@@ -80,11 +300,6 @@ public abstract class CharacterBase : NetworkBehaviour
     {
         gameObject.SetActive(false);
     }
-    
-    protected virtual void Update()
-    {
-        if (movement == null) return;
+#endregion
 
-        movement.FixedUpdate();
-    }
 }
