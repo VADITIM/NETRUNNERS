b398c42eb7da60d87b6b26ae56b348bb6a67e80c
Vadim Niedental <vadim.niedental@gmail.com>
2025-02-19 22:54:52 +0100

    Method to mirror object with script.

---
+++ Scripts/Miscellaneous/ObjectReposition.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ObjectReposition : MonoBehaviour
+{
+    private bool done = false;
+
+    void Start()
+    {
+        if (!done)
+        {
+            done = true;
+            RepositionObject();
+        }
+    }
+
+    private void RepositionObject()
+    {
+        GameObject objectToModify = this.gameObject;
+
+        Vector3 newPosition = objectToModify.transform.position;
+        newPosition.x *= -1;
+
+        Quaternion newRotation = objectToModify.transform.rotation;
+        newRotation.eulerAngles = new Vector3(newRotation.eulerAngles.x, newRotation.eulerAngles.y - 180, newRotation.eulerAngles.z);
+
+        GameObject newObject = Instantiate(objectToModify, newPosition, newRotation);
+        newObject.name = objectToModify.name + "_Copy";
+
+        Destroy(newObject.GetComponent<ObjectReposition>());
+
+        // Debug.Log("Object " + objectToModify.name + " has been repositioned to " + newPosition + " and rotated to " + newRotation.eulerAngles);
+    }
+}
