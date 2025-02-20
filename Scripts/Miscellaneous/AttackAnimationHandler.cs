6bc4805926cff23f33d30486399c48175174c24d
Vadim Niedental <vadim.niedental@gmail.com>
2025-02-20 18:50:43 +0100

    Animation Handler to keep track of animation length.

---
+++ Scripts/Miscellaneous/AttackAnimationHandler.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class AttackAnimationHandler : MonoBehaviour
+{
+    private CharacterBase characterBase;
+
+    private void Awake()
+    {
+        characterBase = GetComponent<CharacterBase>();
+    }
+
+    public void StartAttack()
+    {
+
+        if (characterBase != null)
+        {
+            characterBase.StartNormalAttack();
+            characterBase.StartSpecialAttack();
+            characterBase.StartAirNormalAttack();
+            characterBase.StartAirSpecialAttack();
+        }
+    }
+
+    public void EndAttack()
+    {
+        if (characterBase != null)
+        {
+            characterBase.EndNormalAttack();
+            characterBase.EndSpecialAttack();
+            characterBase.EndAirNormalAttack();
+            characterBase.EndAirSpecialAttack();
+        }
+    }
+}
