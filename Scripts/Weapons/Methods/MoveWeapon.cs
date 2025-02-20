84c2a2395e5a9fdf5ef610e4be95c5785e3f7e68
Vadim Niedental <vadim.niedental@gmail.com>
2025-02-20 18:50:12 +0100

    Method to move the weapon on the Y axis.

---
+++ Scripts/Weapons/Methods/MoveWeapon.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using FishNet.Object;
+
+public class MoveWeapon : NetworkBehaviour
+{
+    public float lastPosY { get; private set; }
+    
+    private const float SNAPDISTANCE = .3f;
+    private const float MAXMOVEDISTANCE = .6f;
+    private float currentYOffset = 0f;
+    
+    private WeaponBase weaponBase;
+
+    public override void OnStartClient()
+    {
+        weaponBase = GetComponent<WeaponBase>();
+    }
+
+    public void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        if (!weaponBase || !base.HasAuthority || weaponBase.weaponHolder == null || weaponBase.owner == null) return;
+        
+        float baseY = weaponBase.owner.transform.position.y + .5f;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && currentYOffset < MAXMOVEDISTANCE)
+        {
+            currentYOffset += SNAPDISTANCE;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && currentYOffset > -MAXMOVEDISTANCE)
+        {
+            currentYOffset -= SNAPDISTANCE;
+        }
+
+        lastPosY = baseY + currentYOffset;
+        MoveServer(lastPosY);
+    }
+
+    [ServerRpc]
+    private void MoveServer(float newYPosition)
+    {
+        if (!weaponBase || weaponBase.weaponHolder == null) return;
+
+        lastPosY = newYPosition;
+        weaponBase.weaponHolder.position = new Vector3(weaponBase.weaponHolder.position.x, newYPosition, weaponBase.weaponHolder.position.z);
+
+        MoveClient(newYPosition);
+    }
+
+    [ObserversRpc]
+    private void MoveClient(float newYPosition)
+    {
+        if (!weaponBase || weaponBase.weaponHolder == null) return;
+
+        lastPosY = newYPosition;
+        weaponBase.weaponHolder.position = new Vector3(weaponBase.weaponHolder.position.x, newYPosition, weaponBase.weaponHolder.position.z);
+    }
+}
